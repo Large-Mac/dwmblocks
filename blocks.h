768d8606394be9557073c91662314e9bccdc0ef3
bighaodwofad@gmail.com <bighaodwofad@gmail.com>
2026-03-13 23:10:44 -0700

    fixed missing icons causing errors

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    { "/etc/scripts/dwmblocks/sb-music",   0,    1  },
-    { "/etc/scripts/dwmblocks/sb-gpu",     5,    0  },
-    { "/etc/scripts/dwmblocks/sb-sysres",  5,    0  },
-    { "/etc/scripts/dwmblocks/sb-temp",    10,   0  },
-    { "/etc/scripts/dwmblocks/sb-date",    60,   0  },
+    { "", "/etc/scripts/dwmblocks/sb-music",   0,    1  },
+    { "", "/etc/scripts/dwmblocks/sb-gpu",     5,    0  },
+    { "", "/etc/scripts/dwmblocks/sb-sysres",  5,    0  },
+    { "", "/etc/scripts/dwmblocks/sb-temp",    10,   0  },
+    { "", "/etc/scripts/dwmblocks/sb-date",    60,   0  },
 };
+
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = "  |  ";
 static unsigned int delimLen = 5;
